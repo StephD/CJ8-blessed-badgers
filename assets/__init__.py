cbb84a7f7ad25910b9c55dcc03740d44b81142d4
Shakya Majumdar <shakyathegreat.majumdar@gmail.com>
2021-07-11 14:25:03 +0530

    lint code

--- assets/__init__.py
+++ assets/__init__.py
@@ -3,9 +3,12 @@ from pathlib import Path
 path = Path("assets")
 
 
-def load_asset(filename: str) -> list:
-    try:
-        with open(path / filename, encoding="utf-8") as f:
-            return f.read().splitlines()
-    except OSError:
-        raise
+def load_asset(filename: str) -> list[str]:
+    """
+    Load an asset from the assets/ directory.
+
+    :param filename: name of the file to be loaded
+    :return:  a list representing the lines of the asset
+    """
+    with open(path / filename, encoding="utf-8") as file:
+        return file.read().splitlines()
