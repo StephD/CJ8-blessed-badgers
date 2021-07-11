2bba5137992b66060693f61440db6a31d016866a
DepSteph <logda@hotmail.com>
2021-07-11 16:18:11 +0800

    First structure

---
+++ assets/modules/menu.py
@@ -0,0 +1,9 @@
+class Menu:
+    """
+    This class will handle the menu and interaction.
+
+    It will allow to navigate at the beginning of the game.
+    """
+
+    def __init__(self) -> None:
+        pass
---
+++ assets/modules/scene.py
@@ -0,0 +1,9 @@
+class Scene:
+    """
+    Scene This class is the definition of a room, a location where the player can enter or exit.
+
+    This scene will be filled with entities
+    """
+
+    def __init__(self) -> None:
+        pass
