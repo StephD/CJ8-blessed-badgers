0693ce80ec1f979a23ab0044e2870c3814843af1
DepSteph <logda@hotmail.com>
2021-07-12 23:04:25 +0800

    refactor screen

--- screens/about_screen.py
+++ screens/about_screen.py
@@ -1,6 +1,7 @@
 import blessed
 import numpy as np
 
+# from modules.language import Language
 from scenes import make_title
 
 from .menu_screen import MenuScreen
@@ -12,33 +13,23 @@ class AboutScreen(MenuScreen):
         cols, rows = term.width, term.height
         rows -= 2
         cols -= 2
+        row, col = np.indices((rows, cols))
+        row = row[::-1]
+        row = (row - rows // 2) * 1 / 2
+        col = (col - cols // 2) / 3 * 1 / 2
 
         with term.cbreak(), term.hidden_cursor():
-            row, col = np.indices((rows, cols))
-            row = row[::-1]
-            row = (row - rows // 2) * 1 / 2
-            col = (col - cols // 2) / 3 * 1 / 2
-
-            old_indices = set()
-
             print(term.home + term.clear)
 
             title_indices = make_title(term, rows, cols)
             menu_indices = self.make_menu(term, rows, cols)
 
+            old_indices = set()
             key_input = ""
             while key_input.lower() not in ["b"]:
                 key_input = term.inkey(timeout=0.02)
-                indices_to_be_painted = set()
-                for square in self.squares:
-                    square.update(((col[0, 0], row[-1, 0]), (col[0, -1], row[0, 0])))
-                    indices_to_be_painted |= square.to_be_painted(row, col)
-                for y_index, x_index in indices_to_be_painted - old_indices - title_indices - menu_indices:
-                    print(term.move_xy(x_index, y_index) + chr(8226), end="", flush=True)
-                for y_index, x_index in old_indices - indices_to_be_painted - title_indices - menu_indices:
-                    print(term.move_xy(x_index, y_index) + " ", end="", flush=True)
-                old_indices = indices_to_be_painted
 
+                old_indices = self.render_flying_square(term, col, row, old_indices, title_indices, menu_indices)
             return key_input
 
     def make_menu(self, term: blessed.Terminal, rows: int, cols: int) -> set[tuple[int, int]]:
--- screens/menu_screen.py
+++ screens/menu_screen.py
@@ -11,46 +11,31 @@ from scenes.menu_title import make_title
 
 class MenuScreen:
     def __init__(self):
-        num_squares = 6
+        num_squares = 4
         self.squares = [Square() for _ in range(num_squares)]
         self.language = Language()
 
     def render(self, term: blessed.Terminal) -> Union[Keystroke, str]:
         """Renders the start screen in the terminal."""
         cols, rows = term.width, term.height
-        rows -= 2
-        cols -= 2
+        row, col = np.indices((rows, cols))
+        row = row[::-1]
+        row = (row - rows // 2) * 1 / 2
+        col = (col - cols // 2) / 3 * 1 / 2
+        cols, rows = term.width - 2, term.height - 2
 
         with term.cbreak(), term.hidden_cursor():
-            print(term.home + term.clear)
-
-            row, col = np.indices((rows, cols))
-            row = row[::-1]
-            row = (row - rows // 2) * 1 / 2
-            col = (col - cols // 2) / 3 * 1 / 2
-
-            old_indices = set()
+            print(term.home + term.blue + term.clear)
 
             title_indices = make_title(term, rows, cols)
             menu_indices = self.make_menu(term, rows, cols)
 
+            old_indices = set()
             key_input = ""
             while key_input.lower() not in ["n", "c", "t", "a", "q", "l"]:
                 key_input = term.inkey(timeout=0.02)
 
-                # Print the flying square
-                indices_to_be_painted = set()
-                for square in self.squares:
-                    square.update(((col[0, 0], row[-1, 0]), (col[0, -1], row[0, 0])))
-                    indices_to_be_painted |= square.to_be_painted(row, col)
-
-                for y_index, x_index in indices_to_be_painted - old_indices - title_indices - menu_indices:
-                    print(term.move_xy(x_index, y_index) + chr(8226), end="", flush=True)
-
-                for y_index, x_index in old_indices - indices_to_be_painted - title_indices - menu_indices:
-                    print(term.move_xy(x_index, y_index) + " ", end="", flush=True)
-
-                old_indices = indices_to_be_painted
+                old_indices = self.render_flying_square(term, col, row, old_indices, title_indices, menu_indices)
 
             return key_input
 
@@ -74,3 +59,18 @@ class MenuScreen:
             print(menu_item + term.move_right(5), end="")
 
         return {(menu_row, x + menu_start_col) for x in range(menu_width)}
+
+    def render_flying_square(self, term, col, row, old_indices, title_indices, menu_indices) -> set:
+        """It will render the flying square around the screen"""
+        indices_to_be_painted = set()
+        for square in self.squares:
+            square.update(((col[0, 0], row[-1, 0]), (col[0, -1], row[0, 0])))
+            indices_to_be_painted |= square.to_be_painted(row, col)
+
+        for y_index, x_index in indices_to_be_painted - old_indices - title_indices - menu_indices:
+            print(term.move_xy(x_index, y_index) + chr(8226), end="", flush=True)
+
+        for y_index, x_index in old_indices - indices_to_be_painted - title_indices - menu_indices:
+            print(term.move_xy(x_index, y_index) + " ", end="", flush=True)
+
+        return indices_to_be_painted
