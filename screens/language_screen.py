545a11513944afa1286f39999768cc26d111bd9f
DepSteph <logda@hotmail.com>
2021-07-12 23:49:49 +0800

    Language screen with language file + rename print_title

--- screens/language_screen.py
+++ screens/language_screen.py
@@ -1,7 +1,7 @@
 import blessed
 import numpy as np
 
-from scenes import make_title
+from scenes.menu_title import print_title
 
 from .menu_screen import MenuScreen
 
@@ -19,34 +19,38 @@ class LanguageScreen(MenuScreen):
         with term.cbreak(), term.hidden_cursor():
             print(term.home + term.clear)
 
-            title_indices = make_title(term, rows, cols)
-            menu_indices = self.make_menu(term, rows, cols)
+            title_indices = print_title(term, rows, cols)
+            menu_indices = self.print_text(term, rows, cols)
 
             old_indices = set()
             key_input = ""
-            while key_input.lower() not in ["b"]:
+            while key_input.lower() not in ["c"]:
                 key_input = term.inkey(timeout=0.02)
 
                 old_indices = self.render_flying_square(term, col, row, old_indices, title_indices, menu_indices)
 
             return key_input
 
-    def make_menu(self, term: blessed.Terminal, rows: int, cols: int) -> set[tuple[int, int]]:
+    def print_text(self, term: blessed.Terminal, rows: int, cols: int) -> set[tuple[int, int]]:
         """Draws the menu in the terminal, returning the coordinates where it printed."""
-        # divide menu to sections: What, Who, Why
-        en = "Select English" + " " * 5 + "[1]"
-        ru = "выбрать русский" + " " * 4 + "[2]"
-        es = "Elegir español" + " " * 5 + "[3]"
-        fr = "Elija francés" + " " * 6 + "[4]"
-        de = "Wählen Sie Deutsch" + " " + "[5]"
-        exit_text = "Exit" + " " * 15 + "[b]"
-        sections = [en, ru, es, fr, de, exit_text]
+        languages = self.language.get("menu", "languages")
+        langs = [
+            ("{:<15}".format(self.language.get("menu", "languages", lang)) + f"[{i+1}]")
+            for i, lang in enumerate(languages)
+        ]
+
+        langs.append("")
+        langs.append(self.language.get("action", "cancel") + " [C]")
+
         coordinates = set()
-        for i, section in enumerate(sections):
+        for i, section in enumerate(langs):
             menu_width = len(section + " " * 0)
             menu_start_col = (cols - menu_width) // 2
-            menu_row = rows - 10 + i
+            menu_row = rows - (2 + len(langs)) + i
+
             print(term.move_xy(menu_start_col, menu_row), end="")
             print(section)
+
             coordinates = coordinates.union({(menu_row, x + menu_start_col) for x in range(menu_width)})
+
         return coordinates
