594c131f0008b2ab998a18521d68df129e627b71
DepSteph <logda@hotmail.com>
2021-07-12 22:19:46 +0800

    Refactor screen with render text

--- screens/about_screen.py
+++ screens/about_screen.py
@@ -2,7 +2,6 @@ import blessed
 import numpy as np
 
 from scenes import make_title
-from screens.about import make_menu
 
 from .menu_screen import MenuScreen
 
@@ -25,7 +24,7 @@ class AboutScreen(MenuScreen):
             print(term.home + term.clear)
 
             title_indices = make_title(term, rows, cols)
-            menu_indices = make_menu(term, rows, cols)
+            menu_indices = self.make_menu(term, rows, cols)
 
             key_input = ""
             while key_input.lower() not in ["b"]:
@@ -41,3 +40,21 @@ class AboutScreen(MenuScreen):
                 old_indices = indices_to_be_painted
 
             return key_input
+
+    def make_menu(self, term: blessed.Terminal, rows: int, cols: int) -> set[tuple[int, int]]:
+        """Draws the menu in the terminal, returning the coordinates where it printed."""
+        # divide menu to sections: What, Who, Why
+        what = "This is a gamefied learning experience for young developers"
+        who = "Made by young developers: Blessed badgers"
+        why = "For fun and codejam :)"
+        exit_text = "press [b] to return to Main menu"
+        sections = [what, who, why, exit_text]
+        coordinates = set()
+        for i, section in enumerate(sections):
+            menu_width = len(section + " " * 2)
+            menu_start_col = (cols - menu_width) // 2
+            menu_row = rows - 5 + i
+            print(term.move_xy(menu_start_col, menu_row), end="")
+            print(section)
+            coordinates = coordinates.union({(menu_row, x + menu_start_col) for x in range(menu_width)})
+        return coordinates
--- screens/menu_screen.py
+++ screens/menu_screen.py
@@ -6,7 +6,6 @@ from blessed.keyboard import Keystroke
 
 from modules.flying_square import Square
 from scenes.menu_title import make_title
-from screens.menu import make_menu
 
 
 class MenuScreen:
@@ -31,7 +30,7 @@ class MenuScreen:
             old_indices = set()
 
             title_indices = make_title(term, rows, cols)
-            menu_indices = make_menu(term, rows, cols)
+            menu_indices = self.make_menu(term, rows, cols)
 
             key_input = ""
             while key_input.lower() not in ["n", "c", "t", "a", "q", "l"]:
@@ -52,3 +51,24 @@ class MenuScreen:
                 old_indices = indices_to_be_painted
 
             return key_input
+
+    def make_menu(self, term: blessed.Terminal, rows: int, cols: int) -> set[tuple[int, int]]:
+        """Draws the menu in the terminal, returning the coordinates where it printed."""
+        menu_items = [
+            "New Game [n]",
+            "Tutorial [t]",
+            "Continue [c]",
+            "About [a]",
+            "Language [l]",
+            "Quit [q]",
+        ]
+        menu_width = len((" " * 5).join(menu_items))
+        menu_start_col = (cols - menu_width) // 2
+        menu_row = rows - 3
+
+        print(term.move_xy(menu_start_col, menu_row), end="")
+
+        for menu_item in menu_items:
+            print(menu_item + term.move_right(5), end="")
+
+        return {(menu_row, x + menu_start_col) for x in range(menu_width)}
