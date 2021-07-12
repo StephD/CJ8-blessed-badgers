72218e32c76806a753d0082ce3bc5cb488c05656
Anindya-Prithvi <anindya03prithvi@gmail.com>
2021-07-12 11:53:23 +0530

    Language scene selection added with back button

--- modules/lang.py
+++ modules/lang.py
@@ -19,19 +19,20 @@ def make_title(term: blessed.Terminal, rows: int, cols: int) -> set[tuple[int, i
 
 def make_menu(term: blessed.Terminal, rows: int, cols: int) -> set[tuple[int, int]]:
     """Draws the menu in the terminal, returning the coordinates where it printed."""
-    menu_items = [
-        "New Game [n]",
-        "Tutorial [t]",
-        "Continue [c]",
-        "About [a]",
-        "Language [l]",
-        "Quit [q]",
-    ]
-    menu_width = len((" " * 5).join(menu_items))
-    menu_start_col = (cols - menu_width) // 2
-    menu_row = rows - 3
-    print(term.move_xy(menu_start_col, menu_row), end="")
-    for menu_item in menu_items:
-        print(menu_item + term.move_right(5), end="")
-
-    return {(menu_row, x + menu_start_col) for x in range(menu_width)}
+    # divide menu to sections: What, Who, Why
+    en = "Select English" + " "*5 + "[1]"
+    ru = "выбрать русский" + " "*4 + "[2]"
+    es = "Elegir español" + " "*5 + "[3]"
+    fr = "Elija francés" + " "*6 + "[4]"
+    de = "Wählen Sie Deutsch" + " " + "[5]"
+    exit_text = "Exit" + " "*15 + "[b]"
+    sections = [en, ru, es, fr, de, exit_text]
+    coordinates = set()
+    for i, section in enumerate(sections):
+        menu_width = len(section + " " * 0)
+        menu_start_col = (cols - menu_width) // 2
+        menu_row = rows - 14 + i
+        print(term.move_xy(menu_start_col, menu_row), end="")
+        print(section)
+        coordinates = coordinates.union({(menu_row, x + menu_start_col) for x in range(menu_width)})
+    return coordinates
